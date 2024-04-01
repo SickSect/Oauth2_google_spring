720d95a37b52b94d026da0c29dabecba4c1a6ff6
Ugina <sleepyboy906sb@gmail.com>
2024-04-01 22:49:41 +0300

    fix

--- demo/src/main/java/com/oauth/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/oauth/demo/config/SecurityConfig.java
@@ -5,17 +5,30 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Collections;
 
 @Configuration
 @Slf4j
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .build();
+    }
 
     @Bean
     BCryptPasswordEncoder encoder(){
